07d5d7d9a18ce9c3af60de6a1c37717c8559d0a7
Crowdbotics <engineering@crowdbotics.com>
2023-03-15 11:34:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lively_violet_dev_69657/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lively_violet_dev_69657;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "lively_violet_dev_69657";
+  }
+}
